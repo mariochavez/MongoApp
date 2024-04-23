eec19db1fe5003ada3c323a355153941f98d477e
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-04-23 12:40:19 -0600

    Update for newer MongoDB, MacOS, and Xcode

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,56 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        curl: {
-            mongodb: {
-              src: 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-<%= pkg.mongo_version %>.tgz',
-              dest: 'tmp/mongodb-osx-x86_64-<%= pkg.mongo_version %>.tgz'
-            },
-        },
-        copy: {
-          mongodb: {
-            files: [{
-                expand: true,
-                cwd: 'tmp/mongodb-osx-x86_64-<%= pkg.mongo_version %>/',
-                src: ['**'],
-                dest: 'mongodb/',
-                mode: true
-            }]
-          }
-        },
-        exec: {
-            unzip: {
-                cmd: 'cd ./tmp && tar zxvf mongodb-osx-x86_64-<%= pkg.mongo_version %>.tgz',
-                // cwd: '.',
-                stdout: true,
-                stderr: true
-            },
-            chmod: {
-                cmd: 'chmod +x ./mongodb/bin/*',
-                // cwd: '.',
-                stdout: true,
-                stderr: true
-            },
-            build: {
-                cmd: 'xcodebuild',
-                // cwd: '.',
-                stdout: true,
-                stderr: true
-            },/**/
-        },
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    curl: {
+      mongodb: {
+        src: 'http://fastdl.mongodb.org/osx/mongodb-macos-arm64-<%= pkg.mongo_version %>.tgz',
+        dest: 'tmp/mongodb-macos-arm64-<%= pkg.mongo_version %>.tgz'
+      },
+    },
+    copy: {
+      mongodb: {
+        files: [{
+          expand: true,
+          cwd: 'tmp/mongodb-macos-arm64-<%= pkg.mongo_version %>/',
+          src: ['**'],
+          dest: 'mongodb/',
+          mode: true
+        }]
+      }
+    },
+    exec: {
+      unzip: {
+        cmd: 'cd ./tmp && tar zxvf mongodb-macos-arm64-<%= pkg.mongo_version %>.tgz',
+        // cwd: '.',
+        stdout: true,
+        stderr: true
+      },
+      chmod: {
+        cmd: 'chmod +x ./mongodb/bin/*',
+        // cwd: '.',
+        stdout: true,
+        stderr: true
+      },
+      build: {
+        cmd: 'xcodebuild',
+        // cwd: '.',
+        stdout: true,
+        stderr: true
+      },/**/
+    },
 
-    });
+  });
 
-    grunt.loadNpmTasks('grunt-exec');
-    grunt.loadNpmTasks('grunt-curl');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('download', ['curl']);
-    grunt.registerTask('unzip', ['exec:unzip', 'copy:mongodb', 'exec:chmod']);
-    grunt.registerTask('build', ['exec:build']);
+  grunt.registerTask('download', ['curl']);
+  grunt.registerTask('unzip', ['exec:unzip', 'copy:mongodb', 'exec:chmod']);
+  grunt.registerTask('build', ['exec:build']);
 
-    grunt.registerTask('default', ['download', 'unzip', 'build']);
+  grunt.registerTask('default', ['download', 'unzip', 'build']);
 
 };
